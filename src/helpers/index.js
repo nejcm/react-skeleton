52bab98a6dbbe7e1b0763db40f457e5cccee52b4
Nejc <nmursi2@gmail.com>
2020-03-18 02:21:21 +0100

    Updates

--- src/helpers/index.js
+++
@@ -1,8 +0,0 @@
-/**
- *  Convert measure value to valid css
- * @param {String|Number} value Measure value
- * @returns {String} Css value
- */
-export const measure2Css = (value) => {
-  return value ? (typeof value === 'string' ? value : `${value}px`) : undefined;
-};
