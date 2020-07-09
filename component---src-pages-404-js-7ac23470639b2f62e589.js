3d805fa8e86bf866520aa77831fe643e06a9f9ba
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-09 01:00:03 +0000

    Deploy nejcm/react-skeleton to github.com/nejcm/react-skeleton.git:gh-pages

--- component---src-pages-404-js-7ac23470639b2f62e589.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{w2l6:function(t,e,n){"use strict";n.r(e);n("r0ML");var i=n("xH0s");e.default=function(){return Object(i.d)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-7ac23470639b2f62e589.js.map
